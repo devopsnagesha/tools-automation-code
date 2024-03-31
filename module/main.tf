7348e06f2a7623c249b9ae4152ef031440775d34
nagesha technology <asbconstruction19@gmail.com>
2024-04-01 00:15:43 +0530

    Adding roles, instance, Policy prometheus

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
-
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = var.tool_name
   }
@@ -16,3 +16,44 @@ resource "aws_route53_record" "www" {
   ttl     = 30
 }
 
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.tool_name}-inline-policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = var.policy_resource_list
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.tool_name}-role"
+  role = aws_iam_role.role.name
+}
