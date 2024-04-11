8cd9834f00e86bcb1cea89dee487c586a0b60391
nagesha technology <asbconstruction19@gmail.com>
2024-04-11 18:14:47 +0530

    elk installation and elk setup with ansible, pipeline

--- variable.tf
+++ variable.tf
@@ -1,20 +1,25 @@
 variable "tools" {
   default = {
-    prometheus = {
-      instance_type = "t3.small"
-      policy_resource_list = ["ec2:DescribeInstances"]
-    }
-
-    grafana = {
-      instance_type = "t3.small"
-      policy_resource_list = []
-    }
+#     prometheus = {
+#       instance_type = "t3.small"
+#       policy_resource_list = ["ec2:DescribeInstances"]
+#     }
+#
+#     grafana = {
+#       instance_type = "t3.small"
+#       policy_resource_list = []
+#     }
 
     vault = {
       instance_type = "t3.small"
       policy_resource_list = []
     }
+
+    elk = {
+      instance_type = "r7i.large"
+      policy_resource_list = []
   }
+ }
 }
 
 variable "zone_id" {
